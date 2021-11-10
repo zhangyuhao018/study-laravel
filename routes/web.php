8a57bcedbb79af27219fd8c47f0673a094de3ba2
zhangyuhao018 <yuhao@developer.co.jp>
2021-11-10 18:46:48 +0900

    路由系列基础入门篇

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,28 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::view('/', 'welcome',['website'=>'Laravel']);
+Route::view('/welcome', 'welcome',['website'=>'Laravel学院']);
+
+Route::get('hello',function(){
+    return 'hello welcome to laravelAcademy.org';
 });
+
+Route::match(['get','post'],'foo',function(){
+    return 'this is a request from get or post';
+});
+
+Route::any('bar',function(){
+    return 'this is a request from any HTTP verb';
+});
+
+Route::get('form',function(){
+    return '<form method = "POST" action="/foo">' . '<button type="submit"> 提交</button></form?';
+});
+
+
+// Route::get('/user', 'UsersController@index');
